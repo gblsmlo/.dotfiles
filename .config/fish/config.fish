f8f9b1bce404b861f251dc5e6c1f34add9747a68
Gabriel Melo <gabriel_smelo@proton.me>
2025-01-26 15:15:17 -0300

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,9 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 starship init fish | source
 zoxide init --cmd cd fish | source
+fzf --fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
 

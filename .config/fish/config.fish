c8b3580c3eae51a2094cfd1d0f31ff7ebedc6e46
Gabriel Melo <gbsmlo@gmail.com>
2025-07-14 17:43:08 -0300

    Add NVM configuration to Fish shell and update gitconfig

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,29 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set fish_greeting ""
+set -U fish_greeting ""
 
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# SPACEFISH
+set SPACEFISH_PROMPT_ADD_NEWLINE false
+
+# Add Homebrew's bin directory to path so you can use Homebrew's binaries like `brew`
+fish_add_path "/opt/homebrew/bin/"
 
 starship init fish | source
 zoxide init --cmd cd fish | source
 fzf --fish | source
 
-# bun
+# BUN
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
+# ANDROID
+set -x ANDROID_HOME $HOME/Library/Android/sdk
+set -x PATH $PATH $ANDROID_HOME/emulator
+set -x PATH $PATH $ANDROID_HOME/platform-tools
+
+# NVM
+set -x NVM_DIR ~/.nvm
+function nvm
+    bass source /opt/homebrew/opt/nvm/nvm.sh --no-use ';' nvm $argv
+end

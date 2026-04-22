85fe9c5249e6b07a2e788a7d2897ccb6cbf17f6f
Gabriel Melo <gblsmlo@gmail.com>
2026-04-22 11:11:20 -0300

    update fish and zed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,6 +26,11 @@ alias grh="git reset --hard"
 # Github
 alias gclone="gh repo clone"
 
+# pnpm
+alias pn="pnpm"
+alias pni="pnpm install"
+alias pnid="pnpm install -D"
+
 # HTTPie
 alias h="http"
 
@@ -38,12 +43,8 @@ alias opc="opencode"
 # SPACEFISH
 set SPACEFISH_PROMPT_ADD_NEWLINE false
 
-# Add Homebrew's bin directory to path so you can use Homebrew's binaries like `brew`
-fish_add_path "/opt/homebrew/bin/"
-
 starship init fish | source
 zoxide init --cmd cd fish | source
-fzf --fish | source
 
 # BUN
 set --export BUN_INSTALL "$HOME/.bun"
@@ -54,47 +55,6 @@ set -x ANDROID_HOME $HOME/Library/Android/sdk
 set -x PATH $PATH $ANDROID_HOME/emulator
 set -x PATH $PATH $ANDROID_HOME/platform-tools
 
-# NVM
-set -x NVM_DIR ~/.nvm
-function nvm
-    bass source /opt/homebrew/opt/nvm/nvm.sh --no-use ';' nvm $argv
-end
-
-# Auto-load nvm default version
-function __nvm_auto_load
-    if test -s "$NVM_DIR/nvm.sh"
-        bass source /opt/homebrew/opt/nvm/nvm.sh --no-use ';' nvm use default
-    end
-end
-
-# Load nvm default version on shell start
-if status is-interactive
-    __nvm_auto_load
-end
-
-
-# pnpm
-set -gx PNPM_HOME "/Users/gabs/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-# Added by OrbStack: command-line tools and integration
+# ORBSTACK
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-
-# Added by Antigravity
-fish_add_path /Users/gabs/.antigravity/antigravity/bin
-
-# Added by Antigravity
-fish_add_path /Users/gabs/.antigravity/antigravity/bin
-
-# Added by Antigravity
-fish_add_path /Users/gabs/.antigravity/antigravity/bin
-
-# Added by Antigravity
-fish_add_path /Users/gabs/.antigravity/antigravity/bin
-
-# Added by Antigravity
-fish_add_path /Users/gabs/.antigravity/antigravity/bin
